3257dca27207af3bcef7d92ca975749ec463cacc
mlarkin1 <53284697+mlarkin1@users.noreply.github.com>
2023-11-16 08:48:58 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -11,10 +11,16 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(document.querySelector('.guess').value, `This is the value`);
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = ' Correct Number! 🎱';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = ' Too High!📈';
@@ -24,6 +30,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game! 💥';
       document.querySelector('.score').textContent = 0;
     }
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = ' Too Low!📉';
