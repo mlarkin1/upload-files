cae3ca56094524c638a920a53d30356476f3b133
mlarkin1 <53284697+mlarkin1@users.noreply.github.com>
2023-11-16 08:39:07 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -11,18 +11,27 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(document.querySelector('.guess').value, `This is the value`);
-
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = ' Correct Number! 🎱';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = ' Too High!📈';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' Too High!📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 💥';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = ' Too Low!📉';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' Too Low!📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 💥';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
